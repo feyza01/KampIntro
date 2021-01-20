77b34b0e7f87e47b4359d79df4d202e5cbf2fc3f
feyza ülkü şafak <feyzasafak@hacettepe.edu.tr>
2021-01-21 01:15:01 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         // Type safety - tip güvenliği
+         // Do not repeat yourself - Kendini tekrarlama!
+         // değer tutucu, alias
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Eşittir/Değişmedi Butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+         
+
+
+
+
+
+        }
+    }
+}
---
+++ Metotlarr/SepetManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlarr
+{
+    class SepetManager
+    {
+        //naming convention
+        //syntax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete Eklendi : " + urun.Adi);
+
+
+        }
+        public void Ekle2(string urunAdi, string Aciklama, double Fiyat, int StokAdedi)
+        {
+            Console.WriteLine("Sepete eklendi" + urunAdi);
+        }
+
+
+    }
+
+}
